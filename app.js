ea33d3da6198c6812a51a454965ad9f94d45068d
Denzel <denzeltlee@gmail.com>
2020-04-14 11:53:01 +0800

    Finished app.js

---
+++ app.js
@@ -0,0 +1,77 @@
+class Media {
+    constructor(title) {
+        this._title = title;
+        this._isCheckedOut = false;
+        this._ratings = [];
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    get isCheckedOut() {
+        return this._isCheckedOut;
+    }
+
+    set isCheckedOut(newIsCheckedOut) {
+        this.isCheckedOut = newIsCheckedOut;
+    }
+
+    get ratings() {
+        return this._ratings;
+    }
+
+    toggleCheckOutStatus() {
+        this._isCheckedOut = !this._isCheckedOut;
+    }
+
+    getAverageRating() {
+        const array = this.ratings;
+        const summedArr = array.reduce((acc, cv) => acc + cv);
+        const result = summedArr / array.length;
+        return result;
+    }
+
+    addRating(rating) {
+        this.ratings.push(rating);
+    }
+}
+
+class Book extends Media {
+    constructor(title, author, pages) {
+        super(title);
+        this._author = author;
+        this._pages = pages;
+    }
+
+    get author() {
+        return this._author;
+    }
+
+    get pages() {
+        return this._pages;
+    }
+}
+
+class Movie extends Media {
+    constructor(title, director, runTime) {
+        super(title);
+        this._director = director;
+        this._runTime = runTime;
+    }
+
+    get director() {
+        return this._director;
+    }
+
+    get runTime() {
+        return this._runTime;
+    }
+}
+
+const historyOfEverything = new Book('A Short History of Nearly Everything', 'Bill Bryson', 544);
+
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating());
